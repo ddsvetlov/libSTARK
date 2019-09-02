38aeda615864346dc1ff4c290e0d0d12ea324dfb
Daniil Svetlov <dankasvetlov@gmail.com>
2019-09-02 11:44:11 +0300

    Add files via upload

--- dd_script/create_asm_file.py
+++ dd_script/create_asm_file.py
@@ -6,14 +6,14 @@ def create_asm_file(cash1,cash2,value):
 	f.write("MOV"+" "+"r1"+" "+"r0"+" " + str(cash1) +'\n') #1 string
 	f.write("MOV"+" "+"r2"+" "+"r0"+" " + str(cash2) + '\n') #2 string
 	f.write("MOV"+" "+"r3"+" "+"r0"+" " + str(value) + '\n') #3 string
-	#add flag for answer(6 string)
+	#add flag for answer
 	f.write("MOV"+" "+"r8"+" "+"r0"+" "+"1"+'\n') #4 string
 	#here add total cash =cash1+cash2=r4
 	f.write("ADD"+" "+"r4"+" "+"r1"+" "+"r2"+'\n') #5 string
 	#here check that cash1>=value 
-	f.write("CMPAE"+" "+"r1"+" "+"r3"+" "+"r0"+'\n') #6 string
+	f.write("CMPAE"+" "+"r0"+" "+"r1"+" "+"r3"+'\n') #6 string
 	#if not compare jump to end(where answer), last param is number of string where answer is
-	f.write("CNJMP"+" "+"r0"+" "+"r0"+" "+"14"+'\n') #7 string
+	f.write("CNJMP"+" "+"r0"+" "+"r0"+" "+"16"+'\n') #7 string
 	#else make change
 	#here r5=cash1-value=new cash1
 	f.write("SUB"+" "+"r5"+" "+"r1"+" "+"r3"+'\n') #8 string
@@ -21,14 +21,18 @@ def create_asm_file(cash1,cash2,value):
 	f.write("ADD"+" "+"r6"+" "+"r2"+" "+"r3"+'\n') #9 string
 	#here new toal cash=new cash1+new cash2
 	f.write("ADD"+" "+"r7"+" "+"r5"+" "+"r6"+'\n') #10 string
-	#here check that total cash old=total cash new
-	f.write("CMPAE"+" "+"r4"+" "+"r7"+" "+"r0"+'\n') #11 string
+	#here check that total cash old=total cash n
+	f.write("CMPAE"+" "+"r0"+" "+"r4"+" "+"r7"+'\n') #11 string
 	#if not compare jump to end(where answer), last param is number of string where answer is
-	f.write("CNJMP"+" "+"r0"+" "+"r0"+" "+"14"+'\n') #12 string
+	f.write("CNJMP"+" "+"r0"+" "+"r0"+" "+"16"+'\n') #12 string
 	#else change value of flag
 	f.write("MOV"+" "+"r8"+" "+"r0"+" "+"0"+'\n') #13 string
+	#and store new cash1 value into 1 memory
+	f.write("STOREW"+" "+"r5"+" "+"r0"+" "+"1"+'\n') #14 string
+	#and store new cash2 value into 2 memory
+	f.write("STOREW"+" "+"r6"+" "+"r0"+" "+"2"+'\n') #15 string
 	#this is numberstring to jump and answer
-	f.write("ANSWER"+" "+"r0"+" "+"r0"+" "+"r8") #14 string
+	f.write("ANSWER"+" "+"r0"+" "+"r0"+" "+"r8") #16 string
 
 
 
--- dd_script/test_script.py
+++ dd_script/test_script.py
@@ -1,38 +1,162 @@
 import subprocess
+import linecache
+import os
 import create_asm_file
+import test_string_work
+import test_db_script
+import time
 #add testing security params
-t=3
-s=3
-#add testing transaction data
-asm_cash1=10
-asm_cash2=10
-asm_value=50
-flag=False
+security_value1=5
+security_value2=10
+security_param1='-t'+str(security_value1)  
+security_param2='-s'+str(security_value2)
+#add params for fixed string
+opportiunityTrsnsactionStringNumber = 4;
+valueOfFirstUsersCashStringNumber = 2;
+valueOfSecondUsersCashStringNumber = 3;
+stringOpportunity = "can make this transaction"
+stringCash1Check = "new cash is : "
+stringCash2Check = "new cash is : "
+#add testing array data
+array_cash = [20,20,20,20];
+backup_array_cash = array_cash[:];
+array_value_of_transaction= [10,6,58,2,3,4];
 
-f=open("condoub.txt", 'w') #create .txt with console duplication for parcing
-value1='-t'+str(t)  
-value2='-s'+str(s)
-#create  asm file
-create_asm_file.create_asm_file(asm_cash1, asm_cash2, asm_value)
-#show asm file
-tes=open("test.asm")
-for line in tes:
-	print(line)
+startTime = time.time()
 
-#run tinyram
-subprocess.call(["./../stark-tinyram", "test.asm", value1, value2], stdout=f)
-#all info after finish tinyRAM with params t,s now in condoub.txt, find here ACCEPT
-f.close()
-f=open("condoub.txt", 'r')
-for line in f:
-	if "can make this transaction" in line:
-		flag =True
+test_db_script.cleanUpForTesting()
+test_db_script.addBackupData()
+quantity_transaction = test_db_script.getQuantityTransaction()
+# print("suck qua ", quantity_transaction)
 
-if flag==True:
-	#print("old status",asm_cash1,asm_cash2)
-	asm_cash1-=asm_value
-	asm_cash2+=asm_value
-	#print("new status",asm_cash1,asm_cash2)
-	print("can make transaction")
-else:
-	print("ERROR: Can't make transaction")
+#-------------------------------------------------------------------------------------------
+#try to include db
+for i in range(quantity_transaction):
+	print("transaction", i)
+	data = test_db_script.getData(i)
+	fromID = data[0]
+	toID = data[1]
+	cashFrom = data[2]
+	cashTo = data[3]
+	cashValue = data [4]
+	cashID = data [5]
+	# print("data suck", data)
+	# print(fromID, toID, cashFrom, cashTo, cashValue)
+
+	#------------------------------------------------------------------------
+	#start tinyRAM with db values
+
+	#create  asm file
+	create_asm_file.create_asm_file(cashFrom, cashTo, cashValue)
+	#create .txt with console duplication for parcing
+	f=open("condoub.txt", 'w') 	
+	#run tinyram
+	subprocess.call(["./../tinyram/stark-tinyram/stark-tinyram", "test.asm", security_param1, security_param2], stdout=f)
+	f.close()
+
+	#get string from tunyRAM output
+	inputOpportunityString = linecache.getline('condoub.txt', opportiunityTrsnsactionStringNumber);
+
+	inputFirstCash = linecache.getline('condoub.txt',valueOfFirstUsersCashStringNumber);
+	inputSecondCash = linecache.getline('condoub.txt',valueOfSecondUsersCashStringNumber);
+
+
+	r = open("condoub.txt", 'r')
+	dfghd= r.read()
+	print(dfghd)
+	r.close()
+
+
+	#chech that we "can make this transaction"
+	if inputOpportunityString[:len(inputOpportunityString)-1]==stringOpportunity:
+		# print(i)
+		#check binarystring with user's cash
+		#first user cash 
+		if (inputFirstCash[:14])==stringCash1Check:
+			#add new user's 1 cash 
+			# print("fuckfuck", inputFirstCash)
+			# print("fuckfuckfuck   ", test_string_work.binary_string_to_int(inputFirstCash[14:len(inputFirstCash)-1]))
+			cashFrom=test_string_work.binary_string_to_int(inputFirstCash[14:len(inputFirstCash)-1])
+			# print("fuck 1 fuck", array_cash)
+		#second user cash
+		if inputSecondCash[:14]==stringCash2Check:
+			#add new user's 2 cash
+			cashTo=test_string_work.binary_string_to_int(inputSecondCash[14:len(inputSecondCash)-1])
+			# print("fuck 2 fuck",array_cash)
+			# print("-------------------------------------------")
+		if i==quantity_transaction-1:
+			print("transaction completed successfully")
+			
+		test_db_script.updateDB(cashFrom, cashTo, fromID, toID, cashID)
+
+	else:
+		print("ERROR: can't make transaction")
+		test_db_script.callBackup()
+		break
+
+	
+	os.remove("test.asm")
+	linecache.clearcache()
+	os.remove("condoub.txt")
+test_db_script.deleteBackupData()
+test_db_script.printDB()
+print("time", "--- %s seconds ---" % (time.time() - startTime))
+
+#end of try include db
+#---------------------------------------------------------------------------------------------------------------------------
+
+
+
+# #start use tinyram 
+# for i in range(len(array_value_of_transaction)):
+# 	print(array_cash)
+
+# 	#create  asm file
+# 	create_asm_file.create_asm_file(array_cash[i % len(array_cash)], array_cash[(i+1) % len(array_cash)], array_value_of_transaction[i])
+# 	# create_asm_file.create_asm_file(array_cash[i], array_cash[i+1], array_value_of_transaction[i])
+# 	# print(i, array_cash[i % len(array_cash)], array_cash[(i+1) % len(array_cash)], array_value_of_transaction[i])
+# 	# print(linecache.getline('test.asm', 1))
+# 	# print(linecache.getline('test.asm', 2))
+# 	# print(linecache.getline('test.asm', 3))
+
+# 	#create .txt with console duplication for parcing
+# 	f=open("condoub.txt", 'w') 	
+# 	#run tinyram
+# 	subprocess.call(["./../build/tinyram/stark-tinyram/stark-tinyram", "test.asm", security_param1, security_param2], stdout=f)
+# 	f.close()
+
+# 	#all info after finish tinyRAM now in condoub.txt, start parsing file
+# 	f=open("condoub.txt", 'r')
+
+# 	inputOpportunityString = linecache.getline('condoub.txt', opportiunityTrsnsactionStringNumber);
+# 	inputFirstCash = linecache.getline('condoub.txt',valueOfFirstUsersCashStringNumber);
+# 	inputSecondCash = linecache.getline('condoub.txt',valueOfSecondUsersCashStringNumber);
+# 	#print(inputOpportunityString, "\n", inputFirstCash, "\n", inputSecondCash)
+
+# 	#chech that we "can make this transaction"
+# 	if inputOpportunityString[:len(inputOpportunityString)-1]==stringOpportunity:
+# 		# print(i)
+# 		#check binarystring with user's cash
+# 		#first user cash 
+# 		if (inputFirstCash[:14])==stringCash1Check:
+# 			#add new user's 1 cash 
+# 			array_cash[i % len(array_cash)]=test_string_work.binary_string_to_int(inputFirstCash[14:len(inputFirstCash)-1])
+# 			# print("fuck 1 fuck", array_cash)
+# 		#second user cash
+# 		if inputSecondCash[:14]==stringCash2Check:
+# 			#add new user's 2 cash
+# 			array_cash[(i+1) % len(array_cash)]=test_string_work.binary_string_to_int(inputSecondCash[14:len(inputSecondCash)-1])
+# 			# print("fuck 2 fuck",array_cash)
+# 			# print("-------------------------------------------")
+# 		if i==len(array_value_of_transaction):
+# 			print("transaction completed successfully")
+# 			print(array_cash)
+# 	else:
+# 		print("ERROR: can't make transaction")
+# 		array_cash=backup_array_cash
+# 		print(array_cash) 
+# 		break
+# 	f.close()
+# 	os.remove("test.asm")
+# 	linecache.clearcache()
+# 	os.remove("condoub.txt")
